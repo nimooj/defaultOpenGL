3e64efec401c4e4eed32d8b69bcf317b4e5ba49f
friendsqa <friendsqa@gmail.com>
2019-05-14 16:18:02 +0900

    Initial Commit

---
+++ testParaBody/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testParaBody
+//
+//  Created by mjkang on 2019. 5. 14..
+//  Copyright © 2019년 mjkang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
